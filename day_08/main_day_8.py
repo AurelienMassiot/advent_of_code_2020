d78da28495322f145bb6a4483194dab71de7fd6d
Aurelien Massiot <aurelien.massiot@gmail.com>
2020-12-08 10:34:01 +0100

    add day 8 second star

--- day_08/main_day_8.py
+++ day_08/main_day_8.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def read_text_file_lines(input_file_path):
     with open(input_file_path, 'r') as input_file:
         return [str(line) for line in input_file.read().splitlines()]
@@ -8,13 +11,16 @@ def clean_lines(lines):
     return splitted_lines
 
 
-def process_instruction(instruction, pos, acc):
+def process_instruction(instruction, pos, acc, n_max_lines):
     cmd = instruction[0]
     n = int(instruction[1])
     if cmd == 'nop':
         return pos + 1, acc
     elif cmd == 'acc':
-        return pos + 1, acc + n
+        if pos + 1 >= n_max_lines:
+            return -1, acc + n
+        else:
+            return pos + 1, acc + n
     elif cmd == 'jmp':
         return pos + n, acc
 
@@ -23,23 +29,61 @@ def run_program(lines):
     pos = 0
     acc = 0
     visited = {}
+    n_max_lines = len(lines) - 1
     while True:
         instruction = lines[pos]
-        pos, acc = process_instruction(instruction, pos, acc)
-        if pos in visited:
+        pos, acc = process_instruction(instruction, pos, acc, n_max_lines)
+        if pos in visited or pos == -1:
             break
         visited[pos] = 'visited'
     return pos, acc
 
 
+def run_program_n_times(lines, n):
+    n_max_lines = len(lines)
+    pos = 0
+    acc = 0
+    visited = {}
+    for i in range(1, n):
+        instruction = lines[pos]
+        pos, acc = process_instruction(instruction, pos, acc, n_max_lines)
+        visited[pos] = 'visited'
+        if pos == -1:
+            break
+    return pos, acc
+
+
+def create_new_program(i, original_program):
+    new_program = copy.deepcopy(original_program)
+    if new_program[i][0] == 'nop':
+        new_program[i][0] = new_program[i][0].replace('nop', 'jmp')
+    elif new_program[i][0] == 'jmp':
+        new_program[i][0] = new_program[i][0].replace('jmp', 'nop')
+    return new_program
+
+
+def fix_program(original_program):
+    programs_output = []
+    for i in range(len(original_program)):
+        new_program = create_new_program(i, original_program)
+        if new_program == original_program:
+            continue
+        pos, acc = run_program(new_program)
+        if pos == -1 and acc > 0:
+            programs_output.append((pos, acc))
+    return programs_output
+
+
 def first_star():
-    lines = clean_lines(read_text_file_lines('input.txt'))
-    pos, acc = run_program(lines)
+    original_program = clean_lines(read_text_file_lines('input.txt'))
+    pos, acc = run_program(original_program)
     return pos, acc
 
 
 def second_star():
-    pass
+    original_program = clean_lines(read_text_file_lines('input.txt'))
+    programs_output = fix_program(original_program)
+    return programs_output
 
 
 if __name__ == "__main__":
--- day_08/test_main_day_8.py
+++ day_08/test_main_day_8.py
@@ -1,5 +1,5 @@
-
 from main_day_8 import *
+
 
 def test_process_instruction():
     # Given
@@ -9,13 +9,13 @@ def test_process_instruction():
     instruction_4 = ('jmp', -1)
 
     # Given
-    assert process_instruction(instruction_1, pos=0, acc=0) == (1, 0)
-    assert process_instruction(instruction_2, pos=0, acc=0) == (1, 3)
-    assert process_instruction(instruction_3, pos=0, acc=0) == (3, 0)
-    assert process_instruction(instruction_4, pos=1, acc=0) == (0, 0)
+    assert process_instruction(instruction_1, pos=0, acc=0, n_max_lines=10) == (1, 0)
+    assert process_instruction(instruction_2, pos=0, acc=0, n_max_lines=10) == (1, 3)
+    assert process_instruction(instruction_3, pos=0, acc=0, n_max_lines=10) == (3, 0)
+    assert process_instruction(instruction_4, pos=1, acc=0, n_max_lines=10) == (0, 0)
 
 
-def test_input():
+def test_run_program():
     # Given
     lines = clean_lines(read_text_file_lines('test_input.txt'))
 
@@ -25,3 +25,51 @@ def test_input():
     # Then
     assert pos == 1
     assert acc == 5
+
+
+def test_run_program_n_times_first_star():
+    # Given
+    lines = clean_lines(read_text_file_lines('test_input.txt'))
+
+    # Given
+    pos, acc = run_program_n_times(lines, 8)
+
+    # Then
+    assert pos == 1
+    assert acc == 5
+
+
+def test_run_program_n_times_second_star():
+    # Given
+    lines = clean_lines(read_text_file_lines('test_input_2.txt'))
+
+    # Given
+    pos, acc = run_program_n_times(lines, 8)
+
+    # Then
+    assert pos == -1
+    assert acc == 8
+
+
+def test_run_program_second_star():
+    # Given
+    lines = clean_lines(read_text_file_lines('test_input_2.txt'))
+
+    # Given
+    pos, acc = run_program(lines)
+
+    # Then
+    assert pos == -1
+    assert acc == 8
+
+
+def test_fix_program():
+    # Given
+    original_program = clean_lines(read_text_file_lines('test_input.txt'))
+
+    # Given
+    programs_output = fix_program(original_program)
+
+    # Then
+
+    assert programs_output == [(-1, 8)]
